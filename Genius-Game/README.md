b412fc5f184a51ced26b3466f697152924c4e0c4
ByakkoKa <ph.scezario@gmail.com>
2021-11-22 19:12:21 -0300

    Add Genius Game project to folder.

---
+++ Genius-Game/README.md
@@ -0,0 +1,4 @@
+# Genius Game
+Genius Game making with HTML / CSS / JS
+
+<a href="https://jsfiddle.net/ByakkoKa/1vn58hmz/"> Click here to preview...</a>
---
+++ Genius-Game/js/script.js
@@ -0,0 +1,111 @@
+let order = [];
+let clickedOrder = [];
+let score = 0;
+let time = 250;
+
+//0 - green 
+//1 - red
+//2 - yellow
+//3 - blue
+
+const blue = document.querySelector('.blue');
+const red = document.querySelector('.red');
+const green = document.querySelector('.green');
+const yellow = document.querySelector('.yellow');
+
+// Create random order
+let shuffleOrder = () => {
+    let currentOrder = Math.floor(Math.random() * 4);
+    order[order.length] = currentOrder;
+    clickedOrder = [];
+
+    for(let i in order) {
+        let elementColor = createColorElement(order[i]);
+        
+        setTimeout(() => {
+            lightColor(elementColor, (Number(i) + 1));
+        });
+    }
+};
+
+// highlight next color
+let lightColor = (el, num) => {
+    num = num * (time * 2);
+    setTimeout(() => {
+        el.classList.add('selected');
+    }, num - time);
+    setTimeout(() => {
+        el.classList.remove('selected');
+    }, num + (time - 100));
+};
+
+// check order clicks
+let checkOrder = () => {
+    for(let i in clickedOrder) {
+        if(clickedOrder[i] != order[i]) {
+            gameOver();
+            break
+        }
+    }
+
+    if(clickedOrder.length == order.length) {
+        alert(`Score: ${score}\n You win! Starting another level`);
+        nextLevel();
+    }
+};
+
+// Click function
+let click = (color) => {
+    clickedOrder[clickedOrder.length] = color;
+    createColorElement(color).classList.add('selected');
+
+    setTimeout(() => {
+        createColorElement(color).classList.remove('selected');
+        checkOrder();
+    }, time);
+};
+
+// function color returned
+let createColorElement = (color) => {
+    if(color == 0) return green;
+    else if(color == 1) return red;
+    else if(color == 2) return yellow;
+    else if (color == 3) return blue;
+};
+
+//Next level func
+let nextLevel = () => {
+    score++;
+    shuffleOrder();
+};
+
+//game over func
+let gameOver = () => {
+    alert(`Score: ${score}\n You lose, Click on OK to restart game`);
+    order = [];
+    clickedOrder = [];
+
+    playGame();
+};
+
+// Start game
+let playGame = () => {
+    alert('Welcome to Genius game! Starting Game');
+    score = 0;
+
+    time = Number(prompt('Selected game speed, minimal recommended 100\n Easy > 500\n Normal > 250\n Hard < 150'));
+
+    if(isNaN(time) || time <= 0) {
+        time = 250;
+        alert(`You don't entry a acceptable number, game default speed selected`);
+    }
+
+    nextLevel();
+};
+
+green.addEventListener('click', () => click(0));
+red.addEventListener('click', () => click(1));
+yellow.addEventListener('click', () => click(2));
+blue.addEventListener('click', () => click(3));
+
+playGame();
