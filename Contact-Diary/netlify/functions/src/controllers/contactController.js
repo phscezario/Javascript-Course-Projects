85a43a798466e0c4e6539e533ca66cff89b68609
Paulo Cezario <ph.scezario@gmail.com>
2023-12-29 22:15:55 -0300

    test 23

---
+++ Contact-Diary/netlify/functions/src/controllers/contactController.js
@@ -0,0 +1,80 @@
+const Contact = require('../models/ContactModel');
+
+exports.index = (req, res) => {
+    res.render('contact', { contact: {} });
+};
+
+exports.register = async (req, res) => {
+    try {
+        const contact = new Contact(req.body);
+        await contact.register();
+
+        if (contact.errors.length > 0) {
+            req.flash('errors', contact.errors);
+            req.session.save(() => {
+                return res.redirect('/contact');
+            });
+            return;
+        }
+
+        req.flash('success', 'Contact registered.');
+        req.session.save(() => {
+            return res.redirect('/');
+        });
+        return;
+    } catch (e) {
+        console.log(e);
+        return res.render('404');
+    }
+};
+
+exports.edit = async (req, res) => {
+    if (!req.params.id) return res.render('404');
+
+    const contact = await Contact.getContactById(req.params.id);
+
+    if (!contact) return res.render('404');
+
+    res.render('contact', { contact });
+};
+
+exports.update = async (req, res) => {
+    try {
+        if (!req.params.id) return res.render('404');
+
+        const contact = new Contact(req.body);
+
+        await contact.updateById(req.params.id);
+
+        if (contact.errors.length > 0) {
+            req.flash('errors', contact.errors);
+            req.session.save(() => {
+                return res.redirect('/contact');
+            });
+            return;
+        }
+
+        req.flash('success', 'Contact updated.');
+        req.session.save(() => {
+            return res.redirect(`/contact/edit/${contact.contact._id}`);
+        });
+        return;
+    } catch (e) {
+        console.log(e);
+        return res.render('404');
+    }
+};
+
+exports.delete = async (req, res) => {
+    if (!req.params.id) return res.render('404');
+
+    const contact = await Contact.deleteContactById(req.params.id);
+
+    if (!contact) return res.render('404');
+
+    req.flash('success', 'Contact deleted.');
+    req.session.save(() => {
+        return res.redirect('back');
+    });
+    return;
+};
---
+++ Contact-Diary/netlify/functions/src/middlewares/middleware.js
@@ -0,0 +1,32 @@
+exports.middlewareGlobal = (req, res, next) => {
+    res.locals.errors = req.flash('errors');
+    res.locals.success = req.flash('success');
+    res.locals.user = req.session.user;
+
+    next();
+};
+
+exports.checkCsrfError = (err, req, res, next) => {
+    if (err) return res.render('404');
+
+    next();
+};
+
+// exports.checkCsrfError = (err, req, res, next) => {
+//     if (err && err.code === 'EBADCSRFTOKEN') return res.render('404');
+// };
+
+exports.csrfMiddleware = (req, res, next) => {
+    res.locals.csrfToken = req.csrfToken();
+    next();
+};
+
+exports.loginRequired = (req, res, next) => {
+    if (!req.session.user) {
+        req.flash('errors', 'You need login.');
+        req.session.save(() => res.redirect(process.env.BASEURL + '/login'));
+        return;
+    }
+
+    next();
+};
