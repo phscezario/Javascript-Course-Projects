106667c21b676ea56a91d161c5ec6bdab67d2295
Paulo Cezario <ph.scezario@gmail.com>
2023-12-29 23:04:19 -0300

    Teste 24

--- Contact-Diary/router.js
+++ Contact-Diary/router.js
@@ -6,17 +6,17 @@ const contactController = require('./src/controllers/contactController');
 
 const { loginRequired } = require('./src/middlewares/middleware');
 
-router.get('/', loginRequired, homeController.index);
+router.get('./', loginRequired, homeController.index);
 
-router.get('/login', loginController.index);
-router.post('/login/register', loginController.register);
-router.post('/login/login', loginController.login);
-router.get('/login/logout', loginController.logout);
+router.get('./login', loginController.index);
+router.post('./login/register', loginController.register);
+router.post('./login/login', loginController.login);
+router.get('./login/logout', loginController.logout);
 
-router.get('/contact', loginRequired, contactController.index);
-router.post('/contact/register', loginRequired, contactController.register);
-router.get('/contact/edit/:id', loginRequired, contactController.edit);
-router.post('/contact/edit/:id', loginRequired, contactController.update);
-router.get('/contact/delete/:id', loginRequired, contactController.delete);
+router.get('./contact', loginRequired, contactController.index);
+router.post('./contact/register', loginRequired, contactController.register);
+router.get('./contact/edit/:id', loginRequired, contactController.edit);
+router.post('./contact/edit/:id', loginRequired, contactController.update);
+router.get('./contact/delete/:id', loginRequired, contactController.delete);
 
 module.exports = router;
