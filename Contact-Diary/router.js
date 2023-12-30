7284ad11fc09969e07a816311cf40767da74cdbe
Paulo Cezario <ph.scezario@gmail.com>
2023-12-29 23:14:26 -0300

    Test 25

--- Contact-Diary/router.js
+++ Contact-Diary/router.js
@@ -6,17 +6,17 @@ const contactController = require('./src/controllers/contactController');
 
 const { loginRequired } = require('./src/middlewares/middleware');
 
-router.get('./', loginRequired, homeController.index);
+router.get(process.env.BASEURL + '/', loginRequired, homeController.index);
 
-router.get('./login', loginController.index);
-router.post('./login/register', loginController.register);
-router.post('./login/login', loginController.login);
-router.get('./login/logout', loginController.logout);
+router.get(process.env.BASEURL + '/login', loginController.index);
+router.post(process.env.BASEURL + '/login/register', loginController.register);
+router.post(process.env.BASEURL + '/login/login', loginController.login);
+router.get(process.env.BASEURL + '/login/logout', loginController.logout);
 
-router.get('./contact', loginRequired, contactController.index);
-router.post('./contact/register', loginRequired, contactController.register);
-router.get('./contact/edit/:id', loginRequired, contactController.edit);
-router.post('./contact/edit/:id', loginRequired, contactController.update);
-router.get('./contact/delete/:id', loginRequired, contactController.delete);
+router.get(process.env.BASEURL + '/contact', loginRequired, contactController.index);
+router.post(process.env.BASEURL + '/contact/register', loginRequired, contactController.register);
+router.get(process.env.BASEURL + '/contact/edit/:id', loginRequired, contactController.edit);
+router.post(process.env.BASEURL + '/contact/edit/:id', loginRequired, contactController.update);
+router.get(process.env.BASEURL + '/contact/delete/:id', loginRequired, contactController.delete);
 
 module.exports = router;
